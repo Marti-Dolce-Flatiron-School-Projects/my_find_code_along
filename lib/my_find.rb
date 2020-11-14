a5b2e0a7586030966696bfd94f6da359e71f1989
Martinique <me@martidolce.com>
2020-11-14 10:49:31 -0500

    commit tree 1db91c5fd9221d0b9aa022fdc1700b34f4d0d25e

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
 
+    i += 1
+  end
 end
